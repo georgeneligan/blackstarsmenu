cee4f5592482fdc2004cb6bcc1e1c19cdfd269c4
georgeneligan <108015805+georgeneligan@users.noreply.github.com>
2023-04-12 03:51:27 +0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
-let button = document.querySelector(".button");
-button.addEventListener("click", () => {
-  console.log("clickeddd!!!!!!");
+let menu_button = document.querySelector(".button");
+let nav_menu = document.querySelector(".nav_menu");
+let menu_icon = document.querySelector(".fa-bars");
+menu_button.addEventListener("click", () => {
+  nav_menu.classList.toggle("hidden");
+  menu_icon.classList.toggle("fa-xmark");
 });
