82eee364c2c55d79fb3833f2ff66e1c649350b31
georgeneligan <108015805+georgeneligan@users.noreply.github.com>
2023-04-15 04:13:14 +0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,62 @@
 let menu_button = document.querySelector(".button");
 let nav_menu = document.querySelector(".nav_menu");
 let menu_icon = document.querySelector(".fa-bars");
-let menu_primary = document.querySelector(".menu_primary");
-let menu_second = document.querySelector(".menu_second");
+let cold_food_btn = document.querySelector(".coldfood-btn");
+let salat_btn = document.querySelector(".salat-btn");
 let salat = document.querySelector(".salat");
+let coldfood = document.querySelector(".coldfood");
+let hot_food_btn = document.querySelector(".hotfood-btn");
 let hot_food = document.querySelector(".hotfood");
+let bakedgoods_btn = document.querySelector(".bakedgoods-btn");
+let bakedgoods = document.querySelector(".bakedgoods");
 
-menu_second.addEventListener("click", () => {
+salat_btn.addEventListener("click", () => {
+  coldfood.classList.add("hiddenmenu");
   hot_food.classList.add("hiddenmenu");
+  bakedgoods.classList.add("hiddenmenu");
   salat.classList.remove("hiddenmenu");
-  menu_second.classList.add("activemenu");
-  menu_primary.classList.remove("activemenu");
+  salat_btn.classList.add("activemenu");
+  cold_food_btn.classList.remove("activemenu");
+  hot_food_btn.classList.remove("activemenu");
+  bakedgoods_btn.classList.remove("activemenu");
   nav_menu.classList.add("hidden");
   menu_icon.classList.toggle("fa-xmark");
 });
 
-menu_primary.addEventListener("click", () => {
+cold_food_btn.addEventListener("click", () => {
   salat.classList.add("hiddenmenu");
+  coldfood.classList.remove("hiddenmenu");
+  bakedgoods.classList.add("hiddenmenu");
+  hot_food.classList.add("hiddenmenu");
+  cold_food_btn.classList.add("activemenu");
+  salat_btn.classList.remove("activemenu");
+  hot_food_btn.classList.remove("activemenu");
+  bakedgoods_btn.classList.remove("activemenu");
+  nav_menu.classList.add("hidden");
+  menu_icon.classList.toggle("fa-xmark");
+});
+hot_food_btn.addEventListener("click", () => {
+  salat.classList.add("hiddenmenu");
+  coldfood.classList.add("hiddenmenu");
+  bakedgoods.classList.add("hiddenmenu");
   hot_food.classList.remove("hiddenmenu");
-  menu_primary.classList.add("activemenu");
-  menu_second.classList.remove("activemenu");
+  salat_btn.classList.remove("activemenu");
+  cold_food_btn.classList.remove("activemenu");
+  bakedgoods_btn.classList.remove("activemenu");
+  hot_food_btn.classList.add("activemenu");
+  nav_menu.classList.add("hidden");
+  menu_icon.classList.toggle("fa-xmark");
+});
+
+bakedgoods_btn.addEventListener("click", () => {
+  salat.classList.add("hiddenmenu");
+  coldfood.classList.add("hiddenmenu");
+  hot_food.classList.add("hiddenmenu");
+  bakedgoods.classList.remove("hiddenmenu");
+  salat_btn.classList.remove("activemenu");
+  hot_food_btn.classList.remove("activemenu");
+  cold_food_btn.classList.remove("activemenu");
+  bakedgoods_btn.classList.add("activemenu");
   nav_menu.classList.add("hidden");
   menu_icon.classList.toggle("fa-xmark");
 });
